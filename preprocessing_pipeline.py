a0c9e97d59b0441c245e03365fe564a03a8e0f1d
Christopher Hedemann <cj.hedemann@gmail.com>
2023-02-15 12:46:03 +0100

    Preprocessing pipeline

--- preprocessing_pipeline.py
+++ preprocessing_pipeline.py
@@ -1,10 +1,29 @@
 import pandas as pd
 import numpy as np
 
-from sklearn.preprocessing import OneHotEncoder
+from sklearn.preprocessing import OneHotEncoder, StandardScaler
 from sklearn.impute import SimpleImputer
 from sklearn.pipeline import Pipeline
 from sklearn.compose import ColumnTransformer
+
+
+def convert_0_to_nan(df,features):
+    ''' 
+    Returns a dataframe where zeros in selected features 
+    are replaced with nans
+    '''
+    for feature in features:
+        df[feature][df[feature] == 0.] = np.nan
+    return df
+
+
+def convert_labels(y,bins=[0,12.5,30,np.inf]):
+    ''' 
+    Returns a dataframe where zeros in selected features 
+    are replaced with nans
+    '''
+    target_label = pd.cut(y,bins=bins,labels=False, include_lowest=True)
+    return target_label
 
 
 def test_train_time_split(data,target_name,date_name,test_size=0.25):
@@ -24,18 +43,25 @@ def test_train_time_split(data,target_name,date_name,test_size=0.25):
 def make_pipeline(qnt_cols,cat_cols):
 
     qnt_pipeline = Pipeline([
-        ('impute_qnt', SimpleImputer(strategy='mean'))
-    ])
+        ('scale_qnt', StandardScaler()),
+        ('impute_qnt', SimpleImputer(strategy='median')),
+    ]) # columns with quantitative data
 
     cat_pipeline = Pipeline([
         ('impute_cat', SimpleImputer(strategy='constant', fill_value='missing')),
         ('ohe_cat', OneHotEncoder(handle_unknown='ignore'))
-    ])
+    ]) # columns with categorical data
 
-    preprocessor = ColumnTransformer([
-        ('qnt', qnt_pipeline, qnt_cols),
-        ('cat', cat_pipeline, cat_cols)
-    ])
+    if cat_cols==None:
+            preprocessor = ColumnTransformer([
+            ('qnt', qnt_pipeline, qnt_cols)
+        ]) # only quant to transform
+    
+    else:
+        preprocessor = ColumnTransformer([
+            ('qnt', qnt_pipeline, qnt_cols),
+            ('cat', cat_pipeline, cat_cols)
+        ]) # combining quant and cat to column transformer
 
     return preprocessor
 
